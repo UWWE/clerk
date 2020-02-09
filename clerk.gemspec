e76e5faa5328a76ff28a25bef69dbf9d5e03b253
Kyle Zhao <kzhao.sw@gmail.com>
2020-02-09 12:44:06 -0500

    Pull in some ActiveSupport test helpers

--- clerk.gemspec
+++ clerk.gemspec
@@ -31,6 +31,7 @@ Gem::Specification.new do |spec|
   spec.executables   = ['clerk']
   spec.require_paths = ['lib']
 
+  spec.add_development_dependency 'activesupport'
   spec.add_development_dependency 'bundler', '~> 1.17'
   spec.add_development_dependency 'byebug', '~> 10.0.2'
   spec.add_development_dependency 'minitest', '~> 5.0'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,11 @@ $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'clerk'
 
 require 'minitest/autorun'
+
+require 'active_support/testing/declarative'
+require 'active_support/testing/time_helpers'
+require 'active_support/testing/file_fixtures'
+
 require 'webmock/minitest'
 
 Dir["#{File.expand_path('support', __dir__)}/*.rb"].each do |file|
@@ -12,8 +17,9 @@ end
 
 module Clerk
   class TestCase < ::Minitest::Test
-    extend Testing::Declarative
     include Testing::Path
     include Testing::Fixture
+    include ActiveSupport::Testing::TimeHelpers
+    extend ActiveSupport::Testing::Declarative
   end
 end
